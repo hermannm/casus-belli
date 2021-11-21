b0a3f15a34fa14c0c8fb434cee4fbef83789485c
hermannm <hermann.morkrid@gmail.com>
2021-11-21 02:24:18 +0100

    add move order test

---
+++ tests/attack_tests.go
@@ -0,0 +1,52 @@
+package tests
+
+import (
+	"immerse/hermannia/server/types"
+	"immerse/hermannia/server/utils"
+	"testing"
+)
+
+func TestAttack(t *testing.T) {
+	unit := types.Unit{
+		Type:  types.Footman,
+		Color: types.Yellow,
+	}
+
+	area1 := types.BoardArea{
+		Name:              "area1",
+		ControllingPlayer: types.Yellow,
+		OccupyingUnit:     unit,
+		Forest:            false,
+		Castle:            false,
+		Sea:               false,
+	}
+
+	area2 := types.BoardArea{
+		Name:   "area2",
+		Forest: true,
+		Castle: true,
+		Sea:    false,
+	}
+
+	area1.Neighbors["area2"] = types.Neighbor{
+		Area:        area2,
+		AcrossWater: true,
+	}
+
+	area2.Neighbors["area1"] = types.Neighbor{
+		Area:        area1,
+		AcrossWater: true,
+	}
+
+	order := types.Order{
+		From: area1,
+		To:   area2,
+	}
+
+	result := utils.AttackModifier(order)
+	expected := -2
+
+	if result != expected {
+		t.Errorf("Incorrect attack modifier, got %d, want %d", result, expected)
+	}
+}
