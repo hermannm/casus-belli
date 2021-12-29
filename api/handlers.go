7fff7a45a52a648312842b3735ea53792912e6dd
hermannm <hermann.morkrid@gmail.com>
2021-12-29 19:32:36 +0100

    restructure api package files

---
+++ api/handlers.go
@@ -0,0 +1,171 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+// Registers handlers for the lobby API routes.
+func StartAPI(address string, open bool) {
+	if open {
+		http.HandleFunc("/new", createLobbyHandler)
+	}
+	http.HandleFunc("/join", addPlayer)
+	http.HandleFunc("/info", getLobby)
+	http.HandleFunc("/all", getLobbies)
+	http.ListenAndServe(address, nil)
+}
+
+// Checks the given request for the existence of the provided parameter keys.
+// If all exist, returns the parameters, otherwise returns ok = false.
+func checkParams(res http.ResponseWriter, req *http.Request, keys ...string) (
+	params url.Values, ok bool,
+) {
+	params = req.URL.Query()
+
+	for _, key := range keys {
+		if !params.Has(key) {
+			http.Error(res, "insufficient query parameters", http.StatusBadRequest)
+			return nil, false
+		}
+	}
+
+	return params, true
+}
+
+// Handler for creating lobbies for servers that let users create their own lobbies.
+func createLobbyHandler(res http.ResponseWriter, req *http.Request) {
+	params, ok := checkParams(res, req, "id", "playerIDs")
+	if !ok {
+		return
+	}
+
+	id := params.Get("id")
+	if _, ok := lobbies[id]; ok {
+		http.Error(res, "lobby with ID \""+id+"\" already exists", http.StatusConflict)
+		return
+	}
+
+	playerIDs := params["playerIDs"]
+	if len(playerIDs) < 2 {
+		http.Error(res, "at least 2 player IDs must be provided to lobby", http.StatusBadRequest)
+		return
+	}
+
+	_, err := CreateLobby(id, playerIDs)
+	if err != nil {
+		http.Error(res, "error creating lobby", http.StatusInternalServerError)
+		return
+	}
+
+	res.Write([]byte("lobby created"))
+}
+
+// Utility type for responding to requests for lobby info.
+type lobbyInfo struct {
+	ID                 string          `json:"id"`
+	AvailablePlayerIDs map[string]bool `json:"availablePlayerIDs"`
+}
+
+// Handler for returning information about a given lobby.
+func getLobby(res http.ResponseWriter, req *http.Request) {
+	params, ok := checkParams(res, req, "lobby")
+	if !ok {
+		return
+	}
+
+	lobbyID := params.Get("lobby")
+	lobby, ok := lobbies[lobbyID]
+	if !ok {
+		http.Error(res, "no lobby with id \""+lobbyID+"\"", http.StatusBadRequest)
+		return
+	}
+
+	info, err := json.Marshal(lobbyInfo{
+		ID:                 lobby.ID,
+		AvailablePlayerIDs: lobby.AvailablePlayerIDs(),
+	})
+	if err != nil {
+		http.Error(res, "error in reading lobby \""+lobbyID+"\"", http.StatusInternalServerError)
+	}
+
+	res.Write(info)
+}
+
+// Handler for returning information about all available lobbies.
+func getLobbies(res http.ResponseWriter, req *http.Request) {
+	lobbyInfoList := make([]lobbyInfo, 0, len(lobbies))
+
+	for _, lobby := range lobbies {
+		lobbyInfoList = append(lobbyInfoList, lobbyInfo{
+			ID:                 lobby.ID,
+			AvailablePlayerIDs: lobby.AvailablePlayerIDs(),
+		})
+	}
+
+	info, err := json.Marshal(lobbyInfoList)
+	if err != nil {
+		http.Error(res, "error in reading lobby fetching lobby list", http.StatusInternalServerError)
+	}
+
+	res.Write(info)
+}
+
+// Handler for adding a player to a lobby.
+func addPlayer(res http.ResponseWriter, req *http.Request) {
+	params, ok := checkParams(res, req, "lobby", "player")
+	if !ok {
+		return
+	}
+
+	lobbyID := params.Get("lobby")
+	lobby, ok := lobbies[lobbyID]
+	if !ok {
+		http.Error(res, "no lobby with ID "+lobbyID+" exists", http.StatusBadRequest)
+	}
+
+	playerID := params.Get("player")
+	lobby.Mut.Lock()
+	conn, ok := lobby.Connections[playerID]
+	if !ok {
+		http.Error(res, "invalid player ID", http.StatusBadRequest)
+		lobby.Mut.Unlock()
+		return
+	}
+	if conn.isActive() {
+		http.Error(res, "player ID already taken", http.StatusConflict)
+		lobby.Mut.Unlock()
+		return
+	}
+
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		// Accepts all origins for now, in order to enable clients from other networks.
+		CheckOrigin: func(*http.Request) bool { return true },
+	}
+
+	socket, err := upgrader.Upgrade(res, req, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(res, "unable to establish socket connection", http.StatusInternalServerError)
+		lobby.Mut.Unlock()
+		return
+	}
+
+	conn = Connection{
+		Socket: socket,
+		Active: true,
+	}
+	lobby.Connections[playerID] = conn
+	go conn.Listen()
+
+	res.Write([]byte("joined lobby"))
+
+	lobby.Mut.Unlock()
+	lobby.WG.Done()
+}
--- api/lobby.go
+++ api/lobby.go
@@ -1,11 +1,7 @@
 package api
 
 import (
-	"encoding/json"
 	"errors"
-	"log"
-	"net/http"
-	"net/url"
 	"sync"
 
 	"github.com/gorilla/websocket"
@@ -115,17 +111,6 @@ func (lobby Lobby) AvailablePlayerIDs() map[string]bool {
 	return available
 }
 
-// Registers handlers for the lobby API routes.
-func StartAPI(address string, open bool) {
-	if open {
-		http.HandleFunc("/new", createLobbyHandler)
-	}
-	http.HandleFunc("/join", addPlayer)
-	http.HandleFunc("/info", getLobby)
-	http.HandleFunc("/all", getLobbies)
-	http.ListenAndServe(address, nil)
-}
-
 // Creates a lobby with the given ID.
 // Creates connection slot for each of the given player IDs,
 // and adds an equal number to the lobby's wait group.
@@ -148,34 +133,6 @@ func CreateLobby(id string, playerIDs []string) (*Lobby, error) {
 	return &lobby, nil
 }
 
-// Handler for creating lobbies for servers that let users create their own lobbies.
-func createLobbyHandler(res http.ResponseWriter, req *http.Request) {
-	params, ok := checkParams(res, req, "id", "playerIDs")
-	if !ok {
-		return
-	}
-
-	id := params.Get("id")
-	if _, ok := lobbies[id]; ok {
-		http.Error(res, "lobby with ID \""+id+"\" already exists", http.StatusConflict)
-		return
-	}
-
-	playerIDs := params["playerIDs"]
-	if len(playerIDs) < 2 {
-		http.Error(res, "at least 2 player IDs must be provided to lobby", http.StatusBadRequest)
-		return
-	}
-
-	_, err := CreateLobby(id, playerIDs)
-	if err != nil {
-		http.Error(res, "error creating lobby", http.StatusInternalServerError)
-		return
-	}
-
-	res.Write([]byte("lobby created"))
-}
-
 // Removes a lobby from the lobby map and closes its connections.
 func CloseLobby(id string) error {
 	lobby, ok := lobbies[id]
@@ -191,126 +148,4 @@ func CloseLobby(id string) error {
 	delete(lobbies, id)
 
 	return nil
-}
-
-// Handler for adding a player to a lobby.
-func addPlayer(res http.ResponseWriter, req *http.Request) {
-	params, ok := checkParams(res, req, "lobby", "player")
-	if !ok {
-		return
-	}
-
-	lobbyID := params.Get("lobby")
-	lobby, ok := lobbies[lobbyID]
-	if !ok {
-		http.Error(res, "no lobby with ID "+lobbyID+" exists", http.StatusBadRequest)
-	}
-
-	playerID := params.Get("player")
-	lobby.Mut.Lock()
-	conn, ok := lobby.Connections[playerID]
-	if !ok {
-		http.Error(res, "invalid player ID", http.StatusBadRequest)
-		lobby.Mut.Unlock()
-		return
-	}
-	if conn.isActive() {
-		http.Error(res, "player ID already taken", http.StatusConflict)
-		lobby.Mut.Unlock()
-		return
-	}
-
-	upgrader := websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		// Accepts all origins for now, in order to enable clients from other networks.
-		CheckOrigin: func(*http.Request) bool { return true },
-	}
-
-	socket, err := upgrader.Upgrade(res, req, nil)
-	if err != nil {
-		log.Println(err)
-		http.Error(res, "unable to establish socket connection", http.StatusInternalServerError)
-		lobby.Mut.Unlock()
-		return
-	}
-
-	conn = Connection{
-		Socket: socket,
-		Active: true,
-	}
-	lobby.Connections[playerID] = conn
-	go conn.Listen()
-
-	res.Write([]byte("joined lobby"))
-
-	lobby.Mut.Unlock()
-	lobby.WG.Done()
-}
-
-// Utility type for responding to requests for lobby info.
-type lobbyInfo struct {
-	ID                 string          `json:"id"`
-	AvailablePlayerIDs map[string]bool `json:"availablePlayerIDs"`
-}
-
-// Handler for returning information about a given lobby.
-func getLobby(res http.ResponseWriter, req *http.Request) {
-	params, ok := checkParams(res, req, "lobby")
-	if !ok {
-		return
-	}
-
-	lobbyID := params.Get("lobby")
-	lobby, ok := lobbies[lobbyID]
-	if !ok {
-		http.Error(res, "no lobby with id \""+lobbyID+"\"", http.StatusBadRequest)
-		return
-	}
-
-	info, err := json.Marshal(lobbyInfo{
-		ID:                 lobby.ID,
-		AvailablePlayerIDs: lobby.AvailablePlayerIDs(),
-	})
-	if err != nil {
-		http.Error(res, "error in reading lobby \""+lobbyID+"\"", http.StatusInternalServerError)
-	}
-
-	res.Write(info)
-}
-
-// Handler for returning information about all available lobbies.
-func getLobbies(res http.ResponseWriter, req *http.Request) {
-	lobbyInfoList := make([]lobbyInfo, 0, len(lobbies))
-
-	for _, lobby := range lobbies {
-		lobbyInfoList = append(lobbyInfoList, lobbyInfo{
-			ID:                 lobby.ID,
-			AvailablePlayerIDs: lobby.AvailablePlayerIDs(),
-		})
-	}
-
-	info, err := json.Marshal(lobbyInfoList)
-	if err != nil {
-		http.Error(res, "error in reading lobby fetching lobby list", http.StatusInternalServerError)
-	}
-
-	res.Write(info)
-}
-
-// Checks the given request for the existence of the provided parameter keys.
-// If all exist, returns the parameters, otherwise returns ok = false.
-func checkParams(res http.ResponseWriter, req *http.Request, keys ...string) (
-	params url.Values, ok bool,
-) {
-	params = req.URL.Query()
-
-	for _, key := range keys {
-		if !params.Has(key) {
-			http.Error(res, "insufficient query parameters", http.StatusBadRequest)
-			return nil, false
-		}
-	}
-
-	return params, true
 }
