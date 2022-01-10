43e5d75250f0ba5ce7ae2fd4c95c632e81440b35
hermannm <hermann.morkrid@gmail.com>
2022-01-10 14:15:24 +0100

    add cli for local game server configuration

--- app/local/main.go
+++ app/local/main.go
@@ -1,12 +1,88 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/immerse-ntnu/hermannia/server/api"
+	"github.com/immerse-ntnu/hermannia/server/app"
+	"github.com/immerse-ntnu/hermannia/server/interfaces"
 )
 
+// Launches a game server that runs a single lobby and game.
+// Starts by configuring the lobby and game through a CLI, then listens.
 func main() {
+	fmt.Println("Server started...")
+	fmt.Println()
+
+	game := selectGame(app.Games)
+	fmt.Println()
+
+	createLobby(game)
+	fmt.Println()
+
 	api.RegisterEndpoints(nil)
 	http.ListenAndServe(":7000", nil)
+}
+
+// CLI utility for selecting a game from a list of supported games.
+// Does not return until a valid game selection is made.
+func selectGame(games map[string]interfaces.GameConstructor) interfaces.GameConstructor {
+	fmt.Print("Available games:\n")
+
+	gameTitles := make([]string, 0)
+	for key := range games {
+		gameTitles = append(gameTitles, key)
+	}
+	for index, title := range gameTitles {
+		fmt.Printf("%d: %s\n", index, title)
+	}
+
+	var selectedTitle string
+	for {
+		fmt.Print("Select game (type index or title): ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		selectedTitle = scanner.Text()
+
+		index, err := strconv.Atoi(selectedTitle)
+		if err == nil {
+			selectedTitle = gameTitles[index]
+			break
+		}
+
+		_, ok := games[selectedTitle]
+		if ok {
+			break
+		}
+
+		fmt.Println("Invalid game selection, try again!")
+	}
+
+	fmt.Printf("Selected \"%s\"!\n", selectedTitle)
+	return games[selectedTitle]
+}
+
+// CLI utility for creating a lobby.
+// Does not return until a valid lobby is created.
+func createLobby(game interfaces.GameConstructor) {
+	var lobbyName string
+	for {
+		fmt.Print("Type name of lobby: ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		lobbyName = scanner.Text()
+
+		_, err := api.NewLobby(lobbyName, game)
+		if err == nil {
+			break
+		}
+
+		fmt.Printf("Got error: \"%s\", try again!\n", err.Error())
+	}
+
+	fmt.Printf("Lobby \"%s\" created!\n", lobbyName)
 }
