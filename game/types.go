4082cb6c2766b70f959d3daa887795dd58838bc6
hermannm <hermann.morkrid@gmail.com>
2022-02-24 23:01:10 +0100

    remove pointers from game types

--- game/types.go
+++ game/types.go
@@ -7,7 +7,7 @@ import (
 
 type Game struct {
 	Board    Board
-	Rounds   []*Round
+	Rounds   []Round
 	Lobby    *lobby.Lobby
 	Messages map[Player]*messages.Receiver
 	Options  GameOptions
@@ -21,36 +21,42 @@ type Player string
 
 type Round struct {
 	Season       Season
-	FirstOrders  []*Order
-	SecondOrders []*Order
+	FirstOrders  []Order
+	SecondOrders []Order
 }
 
 type Season string
 
-type Board map[string]*Area
+// A map of area names to areas.
+type Board map[string]Area
 
+// An area on the board map.
 type Area struct {
-	Name             string
-	Nation           string
-	Forest           bool
-	Castle           bool
-	Sea              bool
-	Home             Player
-	Control          Player
-	Unit             Unit
-	SiegeCount       int
-	Battles          []Battle
-	Neighbors        []Neighbor
-	Order            *Order
-	IncomingMoves    []*Order
-	IncomingSupports []*Order
+	Name      string     // Name of the area on the board.
+	Neighbors []Neighbor // Adjacent areas.
+
+	Sea bool // Whether the area is a sea area that can only have ship units.
+
+	Forest bool   // For land areas.
+	Castle bool   // For land areas.
+	Nation string // For land areas.
+	Home   Player // For land areas that are a starting area for a player.
+
+	Unit       Unit   // May change round-to-round.
+	Control    Player // May change round-to-round.
+	SiegeCount int    // For land areas with castles. May change round-to-round.
+
+	Order            Order   // Order for the unit in the area. Resets every round.
+	IncomingMoves    []Order // Incoming move orders to area. Resets every round.
+	IncomingSupports []Order // Incoming support orders to area. Resets every round.
 }
 
+// The relationship between two adjacent areas.
 type Neighbor struct {
-	Area       *Area
-	River      bool
-	Cliffs     bool   // Whether coast between neighboring land areas have cliffs (and thus is impassable to ships).
-	DangerZone string // If not "": the name of the danger zone that the neighboring area lies across (requires check to pass).
+	Name        string // Name of the adjacent area.
+	AcrossWater bool   // Whether a river separates the two areas.
+	Cliffs      bool   // Whether coast between neighboring land areas have cliffs (impassable to ships).
+	DangerZone  string // If not "": the name of the danger zone that the neighboring area lies across (requires check to pass).
 }
 
 type Unit struct {
@@ -63,23 +69,30 @@ type UnitType string
 type Order struct {
 	Type   OrderType
 	Player Player
-	From   *Area
-	To     *Area
-	Via    string
-	Build  UnitType
-	Status OrderStatus
+	Unit   Unit
+	From   string // Name of area where order is placed.
+
+	To string // For move and support orders: Name of destination area.
+
+	Via string // For move orders: Name of DangerZone the order tries to pass through, if any.
+
+	Build UnitType // For build orders: Name of unit type to build.
 }
 
 type OrderType string
 
 type OrderStatus string
 
-type Battle []Result
+type Battle struct {
+	Results    []Result
+	DangerZone string // In case of danger zone crossing: name of the danger zone.
+}
 
 type Result struct {
-	Total  int
-	Parts  []Modifier
-	Player Player
+	Total        int
+	Parts        []Modifier
+	Move         Order
+	DefenderArea string
 }
 
 type Modifier struct {
@@ -89,6 +102,10 @@ type Modifier struct {
 }
 
 type ModifierType string
+
+const ConquerRequirement int = 4
+
+const DangerZoneRequirement int = 3
 
 const Uncontrolled Player = ""
 
@@ -100,7 +117,6 @@ const (
 )
 
 const (
-	NoUnit   UnitType = ""
 	Footman  UnitType = "footman"
 	Horse    UnitType = "horse"
 	Ship     UnitType = "ship"
