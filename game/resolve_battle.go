05e402c30e8a3477ba23d101c6e5f4c3a13a2ed2
hermannm <hermann.morkrid@gmail.com>
2022-02-24 23:04:41 +0100

    update battle resolver

--- game/resolve_battle.go
+++ game/resolve_battle.go
@@ -1,34 +1,49 @@
 package game
 
-func (board Board) resolveBattle(battle Battle) {
+// Resolves effects of the given battle on the board.
+// Forwards the given battle to the appropriate battle resolver based on its type.
+// Returns any retreating move orders that could not be resolved.
+func (board Board) resolveBattle(battle Battle) (retreats []Order) {
 	if battle.IsBorderConflict() {
-		board.resolveBorderConflict(battle)
-		return
+		return board.resolveBorderBattle(battle)
 	}
 
 	if len(battle.Results) == 1 {
-		board.resolvePvEBattle(battle)
-		return
+		return board.resolveSingleplayerBattle(battle)
 	}
 
-	board.resolvePvPBattle(battle)
+	return board.resolveMultiplayerBattle(battle)
 }
 
-func (board Board) resolveBorderConflict(battle Battle) {
+// Resolves effects on the board from the given border battle.
+// Assumes that the battle consists of exactly 2 results, for each of the areas in the battle,
+// that each result is tied to a move order, and that the battle had at least one winner.
+// Returns any retreating move orders that could not be resolved.
+func (board Board) resolveBorderBattle(battle Battle) (retreats []Order) {
 	winners, _ := battle.parseResults()
 	move1 := battle.Results[0].Move
 	move2 := battle.Results[1].Move
 
-	if len(winners) != 1 {
+	// If there is more than one winner, the battle was a tie, and both moves retreat.
+	if len(winners) > 1 {
 		board.removeMove(move1)
 		board.removeMove(move2)
-		return
+
+		if !board.attemptRetreat(move1) {
+			retreats = append(retreats, move1)
+		}
+		if !board.attemptRetreat(move2) {
+			retreats = append(retreats, move2)
+		}
+
+		return retreats
 	}
 
 	winner := winners[0]
 
 	for _, move := range []Order{move1, move2} {
 		if move.Player == winner {
+			// If destination area is uncontrolled, the player must win a singleplayer battle there before taking control.
 			if board[move.To].IsControlled() {
 				board.succeedMove(move)
 			}
@@ -37,86 +52,67 @@ func (board Board) resolveBorderConflict(battle Battle) {
 			board.removeOriginUnit(move)
 		}
 	}
+
+	return nil
 }
 
-func (board Board) resolvePvEBattle(battle Battle) {
+// Resolves effects on the board from the given singleplayer battle (player vs. neutral area).
+// Assumes that the battle has a single result, with a move order tied to it.
+// Returns the move order in a list if it fails retreat, or nil otherwise.
+func (board Board) resolveSingleplayerBattle(battle Battle) (retreats []Order) {
 	winners, _ := battle.parseResults()
 	move := battle.Results[0].Move
 
 	if len(winners) != 1 {
 		board.removeMove(move)
+
+		if board.attemptRetreat(move) {
+			return nil
+		} else {
+			return []Order{move}
+		}
 	}
 
 	board.succeedMove(move)
+	return nil
 }
 
-func (board Board) resolvePvPBattle(battle Battle) {
+// Resolves effects on hte board from the given multiplayer battle.
+// Assumes that the battle has at least 1 winner
+// Returns any retreating move orders that could not be resolved.
+func (board Board) resolveMultiplayerBattle(battle Battle) (retreats []Order) {
 	winners, losers := battle.parseResults()
 	tie := len(winners) != 1
 
 	for _, result := range battle.Results {
+		// If the result has a DefenderArea, it is the result of the area's defender.
+		// If the defender won, nothing changes for them.
+		// If an attacker won, changes to the defender will be handled by calling succeedMove.
+		if result.DefenderArea != "" {
+			continue
+		}
+
 		move := result.Move
 		lost := containsPlayer(losers, move.Player)
 
-		if !lost && !tie {
-			if board[move.To].IsControlled() {
-				board.succeedMove(move)
-			}
-		} else {
+		if lost {
 			board.removeMove(move)
+			board.removeOriginUnit(move)
+			continue
+		}
 
-			if lost {
-				board.removeOriginUnit(move)
+		if tie {
+			board.removeMove(move)
+			if !board.attemptRetreat(move) {
+				retreats = append(retreats, move)
 			}
-		}
-	}
-}
-
-// Parses the results of the battle and finds the winners and losers.
-// In case of a battle against an unconquered area or a danger zone,
-// only one player is returned in one of the lists.
-// In case of a battle between players, multiple winners are returned
-// in the case of a tie.
-func (battle Battle) parseResults() (winners []Player, losers []Player) {
-	// Checks if the battle was against an unconquered area.
-	if len(battle.Results) == 1 {
-		result := battle.Results[0]
-
-		// Order successfully crosses danger zone if it rolls higher than 2.
-		if battle.DangerZone != "" {
-			if result.Total > 2 {
-				return []Player{result.Move.Player}, make([]Player, 0)
-			} else {
-				return make([]Player, 0), []Player{result.Move.Player}
-			}
+			continue
 		}
 
-		// Order conquers uncontrolled area on a result of 4 or higher.
-		if result.Total >= 4 {
-			return []Player{result.Move.Player}, make([]Player, 0)
-		} else {
-			return make([]Player, 0), []Player{result.Move.Player}
+		if board[move.To].IsControlled() {
+			board.succeedMove(move)
 		}
 	}
 
-	// Finds the highest result among the players in the battle.
-	highestResult := 0
-	for _, result := range battle.Results {
-		if result.Total > highestResult {
-			highestResult = result.Total
-		}
-	}
-
-	// Sorts combatants based on whether they exceeded the highest result.
-	winners = make([]Player, 0)
-	losers = make([]Player, 0)
-	for _, result := range battle.Results {
-		if result.Total >= highestResult {
-			winners = append(winners, result.Move.Player)
-		} else {
-			losers = append(losers, result.Move.Player)
-		}
-	}
-
-	return winners, losers
+	return retreats
 }
