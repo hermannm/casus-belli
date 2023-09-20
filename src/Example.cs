003f405262f2c3fa20c59e14d4a156cc577afb48
hermannm <hermann.morkrid@gmail.com>
2023-09-20 15:17:22 +0200

    set up csharpier formatter

--- src/Example.cs
+++ src/Example.cs
@@ -5,17 +5,19 @@ namespace Immerse.BfhClient;
 
 public partial class Example : Sprite2D
 {
-	private double _timePassed;
-	[Export] private double _amplitude = 10.0;
+    [Export]
+    private double _amplitude = 10.0;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		_timePassed += delta;
+    private double _timePassed;
 
-		Position = new Vector2(
-			(float)(_amplitude + _amplitude * Math.Sin(_timePassed * 2.0)),
-			(float)(_amplitude + _amplitude * Math.Cos(_timePassed * 1.5))
-		);
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        _timePassed += delta;
+
+        Position = new Vector2(
+            (float)(_amplitude + _amplitude * Math.Sin(_timePassed * 2.0)),
+            (float)(_amplitude + _amplitude * Math.Cos(_timePassed * 1.5))
+        );
+    }
 }
