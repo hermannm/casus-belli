fe1276fbb32641f737992e00e8d17d94fe21eb0c
hermannm <hermann.morkrid@gmail.com>
2023-11-06 17:58:37 +0100

    implement closing of error messages

--- src/Scenes.cs
+++ src/Scenes.cs
@@ -4,4 +4,7 @@ public static class Scenes
 {
     public const string MainMenu = "res://scenes/main-menu.tscn";
     public const string ServerAddressMenu = "res://scenes/server-address-menu.tscn";
+
+    public const string MessageDisplay = "res://scenes/components/message_display.tscn";
+    public const string ErrorMessage = "res://scenes/components/error_message.tscn";
 }
--- src/UI/MessageDisplay.cs
+++ src/UI/MessageDisplay.cs
@@ -11,17 +11,17 @@ public partial class MessageDisplay : Panel
 
     public override void _Ready()
     {
-        _errorMessageScene = ResourceLoader.Load<PackedScene>(
-            "res://scenes/components/error_message.tscn"
-        );
+        _errorMessageScene = ResourceLoader.Load<PackedScene>(Scenes.ErrorMessage);
 
-        var messageContainerWrapper = ResourceLoader
-            .Load<PackedScene>("res://scenes/components/message_container.tscn")
-            .Instantiate();
+        var messageDisplay =
+            ResourceLoader.Load<PackedScene>(Scenes.MessageDisplay).Instantiate()
+            ?? throw new Exception("Failed to instantiate message display scene");
 
-        _messageContainer = messageContainerWrapper.GetChild<VBoxContainer>(0);
+        _messageContainer =
+            messageDisplay.GetNode<VBoxContainer>("%MessageContainer")
+            ?? throw new Exception("Failed to get message container for message display");
 
-        GetTree().Root.CallDeferred("add_child", messageContainerWrapper);
+        GetTree().Root.CallDeferred("add_child", messageDisplay);
     }
 
     public void ShowError(string errorMessage, params string[] subErrors)
@@ -29,8 +29,12 @@ public partial class MessageDisplay : Panel
         var errorMessageNode = _errorMessageScene.Instantiate();
 
         var label =
-            errorMessageNode.GetNode<Label>("MarginContainer/VBoxContainer/Label")
-            ?? throw new Exception("Failed to get Label for error message node");
+            errorMessageNode.GetNode<Label>("%ErrorMessageLabel")
+            ?? throw new Exception("Failed to get label for error message node");
+
+        var button =
+            errorMessageNode.GetNode<TextureButton>("%CloseErrorButton")
+            ?? throw new Exception("Failed to get close button for error message node");
 
         var stringBuilder = new StringBuilder();
         stringBuilder.Append("Error: ");
@@ -45,5 +49,10 @@ public partial class MessageDisplay : Panel
 
         label.Text = stringBuilder.ToString();
         _messageContainer.AddChild(errorMessageNode);
+
+        button.Pressed += () =>
+        {
+            _messageContainer.RemoveChild(errorMessageNode);
+        };
     }
 }
