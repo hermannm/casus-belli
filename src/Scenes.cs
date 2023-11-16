853b0727d6df65f891c5d9f4773dc3158a71e2cd
hermannm <hermann.morkrid@gmail.com>
2023-11-16 19:30:21 +0100

    switch component scene names to kebab-case for consistency

--- src/Scenes.cs
+++ src/Scenes.cs
@@ -6,6 +6,5 @@ public static class Scenes
     public const string ServerAddressMenu = "res://scenes/server-address-menu.tscn";
     public const string LobbyListMenu = "res://scenes/lobby-list-menu.tscn";
 
-    public const string MessageDisplay = "res://scenes/components/message_display.tscn";
-    public const string ErrorMessage = "res://scenes/components/error_message.tscn";
+    public const string ErrorMessage = "res://scenes/components/error-message.tscn";
 }
