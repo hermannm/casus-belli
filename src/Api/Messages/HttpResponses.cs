70f544fd74d15db7a964fdd9e67ba35c9da31642
hermannm <hermann.morkrid@gmail.com>
2023-11-16 15:07:10 +0100

    match LobbyInfo response struct to match server

--- src/Api/Messages/HttpResponses.cs
+++ src/Api/Messages/HttpResponses.cs
@@ -1,7 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace Immerse.BfhClient.Api.Messages;
 
 public record struct LobbyInfo
 {
-    public required string LobbyName { get; set; }
-    public required string GameName { get; set; }
+    public required string Name { get; set; }
+    public required BoardInfo BoardInfo { get; set; }
+}
+
+public record struct BoardInfo
+{
+    [JsonPropertyName("ID")]
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required int WinningCastleCount { get; set; }
 }
