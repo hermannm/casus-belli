760f36e8827ae922d3690dd7855e356fea42fb02
hermannm <hermann.morkrid@gmail.com>
2023-11-16 20:33:01 +0100

    implement populating lobby list with server response

---
+++ src/Menus/LobbyListMenu/LobbyList.cs
@@ -0,0 +1,74 @@
+using System.Threading.Tasks;
+using Godot;
+using Immerse.BfhClient.Api;
+using Immerse.BfhClient.Api.Messages;
+using Immerse.BfhClient.UI;
+
+namespace Immerse.BfhClient.Menus.LobbyListMenu;
+
+public partial class LobbyList : Node
+{
+    private PackedScene _lobbyListItemScene = ResourceLoader.Load<PackedScene>(
+        Scenes.LobbyListItem
+    );
+    private Popup _usernameInputPopup = null!;
+    private LobbyInfo? _lobbyToJoin = null;
+
+    public override void _EnterTree()
+    {
+        ApiClient.Instance.AddMessageHandler<LobbyJoinedMessage>(HandleLobbyJoinedMessage);
+    }
+
+    public override async void _Ready()
+    {
+        _usernameInputPopup = GetNode<Popup>("%UsernameInputPopup");
+        await PopulateLobbyList();
+        PrepareUsernameInputPopup();
+    }
+
+    private async Task PopulateLobbyList()
+    {
+        var lobbies = await ApiClient.Instance.ListLobbies();
+        if (lobbies is null)
+            return;
+
+        foreach (var lobby in lobbies)
+        {
+            var lobbyListItem = _lobbyListItemScene.Instantiate();
+
+            lobbyListItem.GetNode<Label>("%LobbyName").Text = lobby.Name;
+            lobbyListItem.GetNode<Label>("%GameName").Text = lobby.BoardInfo.Name;
+            lobbyListItem.GetNode<Button>("%JoinButton").Pressed += () =>
+            {
+                _lobbyToJoin = lobby;
+                _usernameInputPopup.PopupCentered();
+            };
+
+            AddChild(lobbyListItem);
+        }
+    }
+
+    private void PrepareUsernameInputPopup()
+    {
+        var usernameInput = _usernameInputPopup.GetNode<LineEdit>("%UsernameInput");
+        var joinLobbyButton = _usernameInputPopup.GetNode<Button>("%JoinLobbyButton");
+
+        joinLobbyButton.Pressed += async () =>
+        {
+            if (_lobbyToJoin is { } lobby)
+            {
+                var username = usernameInput.Text;
+                await ApiClient.Instance.TryJoinLobby(lobby, username);
+            }
+            else
+            {
+                GD.PushError("Lobby to join is not set");
+            }
+        };
+    }
+
+    private static void HandleLobbyJoinedMessage(LobbyJoinedMessage message)
+    {
+        MessageDisplay.Instance.ShowError("Received lobby joined message!");
+    }
+}
--- src/Scenes.cs
+++ src/Scenes.cs
@@ -7,4 +7,5 @@ public static class Scenes
     public const string LobbyListMenu = "res://scenes/lobby-list-menu.tscn";
 
     public const string ErrorMessage = "res://scenes/components/error-message.tscn";
+    public const string LobbyListItem = "res://scenes/components/lobby-list-item.tscn";
 }
