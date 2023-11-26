888b42580c6db11b2e8ae9f2033015cce6f39b03
hermannm <hermann.morkrid@gmail.com>
2023-11-26 16:04:53 +0100

    change Board.discoverCycle to take Region as argument instead of Order

--- server/game/cycles.go
+++ server/game/cycles.go
@@ -2,24 +2,22 @@ package game
 
 // Recursively finds a cycle of move orders through regions starting and ending with the given
 // firstRegionName.
-func (board Board) discoverCycle(firstRegionName RegionName, order Order) (cycle []*Region) {
-	if order.Type != OrderMove {
+func (board Board) discoverCycle(firstRegionName RegionName, region *Region) (cycle []*Region) {
+	if region.order.Type != OrderMove {
 		return nil
 	}
 
-	destination := board[order.Destination]
-
 	// The base case: the destination is the beginning of the cycle.
-	if destination.Name == firstRegionName {
-		return []*Region{destination}
+	if region.order.Destination == firstRegionName {
+		return []*Region{region}
 	}
 
-	// If the base case is not yet reached, passes cycle discovery to the next order in the chain.
-	cycle = board.discoverCycle(firstRegionName, destination.order)
+	// If the base case is not yet reached, passes cycle discovery to the next region in the chain.
+	cycle = board.discoverCycle(firstRegionName, board[region.order.Destination])
 	if cycle == nil {
 		return nil
 	} else {
-		return append(cycle, destination)
+		return append(cycle, region)
 	}
 }
 
